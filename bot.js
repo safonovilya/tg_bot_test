53c07a206cf61337f59854e329fccab04d6d78e9
Илья <tRmWcLxU25PS4U7to6Rw>
2018-04-21 22:47:45 +0700

    new lib and add scenario

--- bot.js
+++ bot.js
@@ -1,37 +1,114 @@
 const TOKEN = process.env.TELEGRAM_TOKEN;
-const TelegramBot = require('node-telegram-bot-api');
-const options = {
-  polling: true
-};
-const bot = new TelegramBot(TOKEN, options);
-const util = require('./util.js')
+const TeleBot = require('telebot');
+const bot = new TeleBot(TOKEN);
+const util = require('./util.js');
 
-// Matches /love
-bot.onText(/\/love/, function onLoveText(msg) {
-  const opts = {
-    reply_to_message_id: msg.message_id,
-    reply_markup: JSON.stringify({
-      keyboard: [
-        ['Yes, you are the bot of my life ❤'],
-        ['No, sorry there is another one...']
-      ]
-    })
-  };
-  bot.sendMessage(msg.chat.id, 'Do you love me?', opts);
-});
-
-bot.onText(/\/getlist/, async function onEchoText(msg, match) {
-  console.log('hello')
-  const opts = {
-    reply_to_message_id: msg.message_id,
-    reply_markup: JSON.stringify({
-      keyboard: await getAvailableDates()
-    })
-  };
-  bot.sendMessage(msg.chat.id, 'what date?', opts);
-});
-
-async function getAvailableDates(){
-  var url =  process.env.URL;
-  return util.getInfo(url)
+async function getAvailableDates() {
+  // TODO
+  // url+'&time_start=yyyy-mm-dd&time_end=yyyy-mm-dd'
+  return util.getInfo(process.env.URL)
 }
+
+//TODO: get from API or DB
+const STUDIOS = ['Аношкина', 'Братьев Кашириных'];
+let list;
+
+// On commands
+bot.on(['/start', '/back'], msg => {
+
+  let replyMarkup = bot.keyboard([
+    ['/записаться', '/список'],
+    ['/hide']
+  ], {resize: true});
+
+  return bot.sendMessage(msg.from.id, 'Начнем', {replyMarkup});
+
+});
+
+bot.on('/записаться', msg => {
+
+  let replyMarkup = bot.keyboard([
+    STUDIOS.map(x => bot.button(x))
+  ], {resize: true, once: true});
+
+  return bot.sendMessage(msg.from.id, 'Вибирете студию', {replyMarkup});
+
+});
+
+bot.on('text', async (msg, props) => {
+  if (STUDIOS.indexOf(msg.text) !== -1) {
+    let keyArray = await getLessonListKB((lesson => {
+      return +lesson.place === STUDIOS.indexOf(msg.text) + 1
+    }))
+    let replyMarkup = bot.inlineKeyboard(keyArray);
+    return bot.sendMessage(msg.from.id, 'Вибири занятие', {replyMarkup});
+  }
+});
+
+// Hide keyboard
+bot.on('/hide', msg => {
+  return bot.sendMessage(
+    msg.from.id, 'Hide keyboard example. Type /back to show.', {replyMarkup: 'hide'}
+  );
+});
+
+// On location on contact message
+bot.on(['location', 'contact'], (msg, self) => {
+  return bot.sendMessage(msg.from.id, `Thank you for ${ self.type }.`);
+});
+
+// Inline buttons
+bot.on('/список', async msg => {
+
+  let keyArray = await getLessonListKB()
+  let replyMarkup = bot.inlineKeyboard(keyArray);
+  return bot.sendMessage(msg.from.id, 'Вибири занятие', {replyMarkup});
+
+});
+
+// Inline button callback
+bot.on('callbackQuery', msg => {
+  // todo delete message
+  // bot.deleteMessage(msg.message.chat.id, msg.message.from.id)
+  let replyMarkup = bot.keyboard([
+    [  bot.button('contact', 'Подтвердить')]
+  ], {resize: true, once: true});
+
+  let lesson = list[+msg.data];
+  let theDate = new Date(lesson['date-start'] * 1000);
+  let date = theDate.toGMTString();
+  return bot.sendMessage(msg.message.chat.id, `Запись к ${lesson.master} на ${ date }`, {replyMarkup});
+});
+
+async function getLessonListKB(filter) {
+
+  list = list || await getAvailableDates();
+  let keyArray = [];
+  let row = [];
+  let count = 0;
+  for (let i in list) {
+    if (!filter || (typeof filter == 'function' && filter(list[i]))) {
+
+      let theDate = new Date(list[i]['date-start'] * 1000);
+
+      //TODO uniq id timestemp + place
+      row.push(
+        bot.inlineButton(
+          (list[i].master + ' ' + theDate.toGMTString()),
+          {callback: i}
+        )
+      );
+      count++
+    }
+    if (count % 2 === 0 || i === list.length - 1) {
+      keyArray.push(row);
+      row = []
+    }
+  }
+  if (row.length) {
+    keyArray.push(row);
+  }
+  return keyArray
+}
+
+bot.start();
--- util.js
+++ util.js
@@ -1,20 +1,13 @@
 const request = require('request');
-const url = 'http://aeroyoga74.ru/schedule';
-const cheerio = require('cheerio');
 
-function getInfo(url){
+async function getInfo(url){
   return new Promise(function (resolve, reject) {
     request.get(url, function (reuest, incomingMessage, body) {
-      const $ = cheerio.load(body)
-      let result = [];
-      $('[id*=schedule]').filter(function () {
-        return $(this).find('.lesson_is_close').length
-      }).each(function () {
-        let time = $(this).find('button h3 strong').text()
-        let title = $(this).find('[name="title"]').text()
-        result.push([time + " " + title])
-      })
-      resolve(result)
+      try {
+        resolve(JSON.parse(body))
+      } catch (e) {
+        reject(e)
+      }
     });
   });
 }
